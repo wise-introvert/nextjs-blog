938695fd03792663456605597708fef6e00355d6
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2021-01-24 21:09:05 +0530

    feat: stylez

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,42 +2,48 @@ const Home: React.FC<{}> = () => (
   <div className={"h-screen flex flex-col justify-center items-center"}>
     <span
       className={
-        "flex flex-col justify-center items-center mb-12  duration-500 rounded hover:bg-black hover:text-white border-2 border-gray-100  shadow-lg cursor-pointer  w-1/2 p-2"
+        "flex flex-col justify-center items-center mb-12  duration-500 rounded   hover:shadow-inner hover:border-0 hover:border-transparent    shadow-md cursor-pointer  w-5/6 md:w-1/2 py-6 p-2"
       }
     >
-      <h1 className={"text-2xl font-serif font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-serif font-light  mb-1"}>
         ABCDEFGHIJKLMNOPQRSTUVWXYZ
       </h1>
-      <h1 className={"text-2xl font-serif font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-serif font-light  mb-1"}>
         abcdefghijklmnopqrstuvwxyz
       </h1>
-      <h1 className={"text-2xl font-serif font-light "}>0987654321</h1>
+      <h1 className={"w-fulltext-xl md:text-2xl font-serif font-light "}>
+        0987654321
+      </h1>
     </span>
     <span
       className={
-        "flex flex-col justify-center items-center mb-12  duration-500 rounded hover:bg-black hover:text-white border-2 border-gray-100  shadow-lg cursor-pointer  w-1/2 p-2"
+        "flex flex-col justify-center items-center mb-12  duration-500 rounded   hover:shadow-inner hover:border-0 hover:border-transparent    shadow-md cursor-pointer  w-5/6 md:w-1/2 py-6 p-2"
       }
     >
-      <h1 className={"text-2xl font-sans font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-sans font-light  mb-1"}>
         ABCDEFGHIJKLMNOPQRSTUVWXYZ
       </h1>
-      <h1 className={"text-2xl font-sans font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-sans font-light  mb-1"}>
         abcdefghijklmnopqrstuvwxyz
       </h1>
-      <h1 className={"text-2xl font-sans font-light "}>0987654321</h1>
+      <h1 className={"w-fulltext-xl md:text-2xl font-sans font-light "}>
+        0987654321
+      </h1>
     </span>
     <span
       className={
-        "flex flex-col justify-center items-center mb-12  duration-500 rounded hover:bg-black hover:text-white border-2 border-gray-100  shadow-lg cursor-pointer  w-1/2 p-2"
+        "flex flex-col justify-center items-center mb-12  duration-500 rounded   hover:shadow-inner hover:border-0 hover:border-transparent    shadow-md cursor-pointer  w-5/6 md:w-1/2 py-6 p-2"
       }
     >
-      <h1 className={"text-2xl font-mono font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-mono font-light  mb-1"}>
         ABCDEFGHIJKLMNOPQRSTUVWXYZ
       </h1>
-      <h1 className={"text-2xl font-mono font-light  mb-1"}>
+      <h1 className={"w-fulltext-xl md:text-2xl font-mono font-light  mb-1"}>
         abcdefghijklmnopqrstuvwxyz
       </h1>
-      <h1 className={"text-2xl font-mono font-light "}>0987654321</h1>
+      <h1 className={"w-fulltext-xl md:text-2xl font-mono font-light "}>
+        0987654321
+      </h1>
     </span>
   </div>
 );
